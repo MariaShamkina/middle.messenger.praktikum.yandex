65f909af4a92dd8a6e5e349dfbdeab691b9e7d3d
Maria Shamkina <murmurxe@yandex.ru>
2022-03-26 16:21:40 +0300

    Настройка деплоя и запуска.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static(`${__dirname}/dist`));
+let dirs = ['chat', 'profile', 'login', 'signin', 'error'];
+dirs.forEach(d => {
+    app.use(express.static(`${__dirname}/dist/${d}`));
+});
+
+app.get('/index.html', (req, res) => {
+    res.redirect("/chat.html");
+});
+
+app.listen(PORT, function () {
+  console.log(`Chat is listening on port ${PORT}!`);
+}); 
